3be6420fc4eaf240a84c26b197ca3129b9982c87
AdinathShinde <adinathshinde54@gmail.com>
2024-07-21 19:08:19 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,63 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+
+   <link rel="icon" href="images/rivalogo.png" type="image/ico">
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/23.png" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Developer's Message:</h3>
+         <p> We are student of master of computer appliction in  Engineering Department from matoshri collage  [Batch : 2024] . we love designing websites and exploring new things. Learning new things.</p>
+
+         <p>I would like to thank collage staff members Sir for guiding me through the session and making me able to develop projects like this. </p>
+         <a href="contact.php" class="btn">Contact Us</a>
+      </div>
+
+   </div>
+
+</section>
+
+
+<?php include 'components/footer.php'; ?>
+
+
+</body>
+</html>
